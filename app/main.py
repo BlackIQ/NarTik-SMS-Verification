96df9b06ea1d93f5a910e5502061ac7d70b97925
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-03 09:24:30 +0430

    Fixing null items

--- app/main.py
+++ app/main.py
@@ -40,24 +40,45 @@ def index() :
 
 @app.route("/pending", methods = ["GET", "POST"])
 def pending() :
-    secret = request.args['secret']
-    phone = request.args['phone']
-    send(secret, phone, "pending")
-    return redirect("/")
+    if request.args['secret'] != None:
+        if request.args['phone'] != None:
+            secret = request.args['secret']
+            phone = request.args['phone']
+            
+            send(secret, phone, "pending")
+            return redirect("/")
+        else:
+            return redirect("/")
+    else:
+        return redirect("/")
 
 @app.route("/confirm", methods=["GET", "POST"])
 def confirm() :
-    secret = request.args['secret']
-    phone = request.args['phone']
-    send(secret, phone, "confirm")
-    return redirect("/")
+    if request.args['secret'] != None:
+        if request.args['phone'] != None:
+            secret = request.args['secret']
+            phone = request.args['phone']
+            
+            send(secret, phone, "confirm")
+            return redirect("/")
+        else:
+            return redirect("/")
+    else:
+        return redirect("/")
 
 @app.route("/reject", methods=["GET", "POST"])
 def reject() :
-    secret = request.args['secret']
-    phone = request.args['phone']
-    send(secret, phone, "reject")
-    return redirect("/")
+    if request.args['secret'] != None:
+        if request.args['phone'] != None:
+            secret = request.args['secret']
+            phone = request.args['phone']
+            
+            send(secret, phone, "reject")
+            return redirect("/")
+        else:
+            return redirect("/")
+    else:
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run()
