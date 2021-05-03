f6f4cd7ac8e02f1db61b32ae68243ad7dfe71afc
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-03 08:10:58 +0430

    Fix args and kavenegar api

--- app/main.py
+++ app/main.py
@@ -1,22 +1,34 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from kavenegar import *
 
 app = Flask(__name__)
+
+api = KavenegarAPI('')
 
 @app.route("/")
 def index() :
     return "index"
 
-@app.route("/pending")
+@app.route("/pending", methods = ["GET", "POST"])
 def pending() :
-    return "Pending"
+    phone = request.args['phone']
+    params = { 'sender' : '1000596446', 'receptor': phone, 'message' :"Flask Test !" }
+    response = api.sms_send( params)
+    return redirect("/")
 
-@app.route("/confirm")
+@app.route("/confirm", methods=["GET", "POST"])
 def confirm() :
-    return "Confirm"
+    phone = request.args['phone']
+    params = { 'sender' : '1000596446', 'receptor': phone, 'message' :"Flask Test !" }
+    response = api.sms_send( params)
+    return redirect("/")
 
-@app.route("/reject")
+@app.route("/reject", methods=["GET", "POST"])
 def reject() :
-    return "Reject"
+    phone = request.args['phone']
+    params = { 'sender' : '1000596446', 'receptor': phone, 'message' :"Flask Test !" }
+    response = api.sms_send( params)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000)
